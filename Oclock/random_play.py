8d706c959e69f21c7d4af372523a8bab8dff7209
Peter <peter.chenxiaofeng@gmail.com>
2019-08-17 20:48:43 +0800

    'main_update'

--- Oclock/random_play.py
+++ Oclock/random_play.py
@@ -7,14 +7,16 @@ pygame.mixer.init()
 pygame.init()
 
 
-def random_play():
-    musics_location = '../musics'
+def random_play(musics_location=None):
+    if not musics_location:
+        musics_location = '../musics'
     musics = os.listdir(musics_location)
     music_locations = [os.path.join(musics_location, i) for i in musics if i.endswith(('.mp3', 'm4a'))]
     music_count = len(music_locations)
-    ran_music = music_locations[random.randint(0, music_count)]
+    ran_music = music_locations[random.randint(0, music_count - 1)]
     print(ran_music)
     track = pygame.mixer.music.load(ran_music)
+
     pygame.mixer.music.play()
     time.sleep(1000)
 
--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import os
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
+from Oclock import random_play
+
+if __name__ == '__main__':
+    BlockScheduler = BlockingScheduler()
+    BackScheduler = BackgroundScheduler()
+    BlockScheduler.add_job(func=random_play.random_play, args=('musics',), trigger='cron', max_instances=10, month='*',
+                           day='*', hour='20', minute='34')
+    BlockScheduler.start()
+    BackScheduler.start()
