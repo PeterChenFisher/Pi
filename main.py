fc70c628058b9b8e2627c4ca1e0afe669395cfc6
PeterPi <peter.chenxiaofeng@gmail.com>
2019-08-17 22:44:18 +0800

    sh file

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     BlockScheduler = BlockingScheduler()
     BackScheduler = BackgroundScheduler()
     BlockScheduler.add_job(func=random_play.random_play, args=('musics',), trigger='cron', max_instances=10, month='*',
-                           day='*', hour='20', minute='34')
+                           day='*', hour='6', minute='45')
     BlockScheduler.start()
     BackScheduler.start()
