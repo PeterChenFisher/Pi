beb3b47698a8110fa05487793f94b9d67dac1bb1
Peter <peter.chenxiaofeng@gmail.com>
2019-08-17 18:36:46 +0800

    'player'

--- Oclock/random_play.py
+++ Oclock/random_play.py
@@ -1,4 +1,23 @@
+import time
+import random
 import pygame
+import os
+
+pygame.mixer.init()
+pygame.init()
+
 
 def random_play():
-    print('begin')
+    musics_location = '..\musics'
+    musics = os.listdir(musics_location)
+    music_locations = [musics_location + '\\' + i for i in musics if i.endswith(('.mp3', 'm4a'))]
+    music_count = len(music_locations)
+    ran_music = music_locations[random.randint(0, music_count)]
+    print(ran_music)
+    track = pygame.mixer.music.load(ran_music)
+    pygame.mixer.music.play()
+    time.sleep(1000)
+
+
+if __name__ == '__main__':
+    random_play()
