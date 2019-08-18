82c39424af67c986a0216fb4d885c9ccf5f31cfd
Peter <peter.chenxiaofeng@gmail.com>
2019-08-18 09:37:33 +0800

    'init-and-reset'

---
+++ Tools/log.py
@@ -0,0 +1,38 @@
+import logging
+from logging.handlers import TimedRotatingFileHandler
+from logging import StreamHandler
+import os
+import config
+
+
+def logger_generator(log_path, logger_name, when='midnight', logger=None):
+    # init
+    if not os.path.exists(log_path):
+        os.mkdir(log_path)
+    # create logger
+    if not logger:
+        logger = logging.getLogger(logger_name)
+    formatter = logging.Formatter(
+        '[%(asctime)s] - [ %(name)s ] - %(levelname)s - %(message)s')
+
+    timedRotatingFileHandler = TimedRotatingFileHandler(filename=os.path.join(log_path, logger_name), when=when,
+                                                        interval=1, backupCount=60, encoding='utf-8')
+    timedRotatingFileHandler.suffix = "%Y-%m-%d.log"
+    timedRotatingFileHandler.setLevel(logging.INFO)
+    timedRotatingFileHandler.setFormatter(formatter)
+
+    streamHandler = StreamHandler()
+    streamHandler.setLevel(logging.INFO)
+    streamHandler.setFormatter(formatter)
+
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(timedRotatingFileHandler)
+    logger.addHandler(streamHandler)
+
+    # logger.info('\n  -Logger %s' % logger.name + 'starts workign.\n' +
+    #             '  Runtime: %s\n' % time.strftime('%Y-%m-%d %H:%M:%S'))
+
+    return logger
+
+
+logger = logger_generator(logger_name='PiProjects', log_path=config.log_path)
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.schedulers.blocking import BlockingScheduler
-from Tools import music_play
+from Tools import music_play, log
+from config import *
+
+logger = log.logger
 
 if __name__ == '__main__':
     BlockScheduler = BlockingScheduler()
     BackScheduler = BackgroundScheduler()
+    BackScheduler._logger = logger
+    BlockScheduler._logger = logger
+    music_play.random_play('musics')
     BlockScheduler.add_job(func=music_play.random_play, args=('musics',), trigger='cron', max_instances=10, month='*',
                            day='*', hour='6', minute='45')
-    BlockScheduler.start()
     BackScheduler.start()
+    BlockScheduler.start()
