99df44bf56a205de0588cf26c20847683e31c3c0
Peter <peter.chenxiaofeng@gmail.com>
2019-08-18 09:57:49 +0800

    'sleep-test'

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ from config import *
 logger = log.logger
 
 if __name__ == '__main__':
+    logger.info('PiProjects Initiating...')
     BlockScheduler = BlockingScheduler()
     BackScheduler = BackgroundScheduler()
     BackScheduler._logger = logger
     BlockScheduler._logger = logger
     time.sleep(100)
-
     music_play.random_play('musics')
+    logger.info('PiProjects Scheduler StartUp!')
     BlockScheduler.add_job(func=music_play.random_play, args=('musics',), trigger='cron', max_instances=10, month='*',
                            day='*', hour='6', minute='45')
     BackScheduler.start()
