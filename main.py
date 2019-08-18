bce4ebf91e022df004535d2880d6cbe1bd755732
Peter <peter.chenxiaofeng@gmail.com>
2019-08-18 09:12:01 +0800

    'reset-music-play'

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import os
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.schedulers.blocking import BlockingScheduler
-from Oclock import random_play
+from Tools import music_play
 
 if __name__ == '__main__':
     BlockScheduler = BlockingScheduler()
     BackScheduler = BackgroundScheduler()
-    BlockScheduler.add_job(func=random_play.random_play, args=('musics',), trigger='cron', max_instances=10, month='*',
+    BlockScheduler.add_job(func=music_play.random_play, args=('musics',), trigger='cron', max_instances=10, month='*',
                            day='*', hour='6', minute='45')
     BlockScheduler.start()
     BackScheduler.start()
