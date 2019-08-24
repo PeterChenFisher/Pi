e53604c4215774e7911383aba76d7f34a1d94cdb
Peter <peter.chenxiaofeng@gmail.com>
2019-08-24 17:11:30 +0800

    'weather-and-player-update'

--- Tools/music_play.py
+++ Tools/music_play.py
@@ -3,6 +3,7 @@ import random
 import pygame
 import os
 from Tools.log import logger
+import cv2
 
 pygame.mixer.init()
 pygame.init()
@@ -10,21 +11,28 @@ pygame.init()
 
 def random_play(musics_location=None):
     if not musics_location:
-        musics_location = '../musics'
+        musics_location = '..\musics'
     musics = os.listdir(musics_location)
     logger.info('Musics:%s' % str(musics))
     music_locations = [os.path.join(musics_location, i) for i in musics if i.endswith(('.mp3', 'm4a'))]
-    music_count = len(music_locations)
-    ran_music = music_locations[random.randint(0, music_count - 1)]
+    ran_music = music_locations[random.randint(0, len(music_locations) - 1)]
     logger.info('Music To Be Played: ' + ran_music)
+    if not play_a_song(ran_music):
+        random_play(musics_location)
+
+
+def play_a_song(music):
     try:
-        track = pygame.mixer.music.load(ran_music)
+        pygame.mixer.music.load(music)
         pygame.mixer.music.play()
+        while (pygame.mixer.music.get_busy()):
+            time.sleep(1)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        return True
     except Exception as e:
         logger.warning('Play Music Failed.Let us Do it Again. Msg:%s' % e)
-        random_play(musics_location)
-        return
-    time.sleep(480)
+        return False
 
 
 if __name__ == '__main__':
