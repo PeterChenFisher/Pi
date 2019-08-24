fef86ce0c4a0181780dc791dc49b8eb3bf2d104c
Peter <peter.chenxiaofeng@gmail.com>
2019-08-24 20:27:36 +0800

    'test'

--- Tools/music_play.py
+++ Tools/music_play.py
@@ -1,4 +1,5 @@
 import time
+import sys
 import random
 import pygame
 import os
@@ -10,16 +11,29 @@ pygame.mixer.init()
 pygame.init()
 
 
-def random_play(musics_location=None):
+def random_play(musics_location=None, mode='pygame'):
     if not musics_location:
-        musics_location = '..\musics'
+        os_platform = sys.platform
+        if os_platform == 'Linux':
+            musics_location = '../musics'
+        elif os_platform == 'win32':
+            musics_location = '..\musics'
+        else:
+            logger.warning('Judge System Failed.Exit.')
+            return
+    print(musics_location)
     musics = os.listdir(musics_location)
     logger.info('Musics:%s' % str(musics))
     music_locations = [os.path.join(musics_location, i) for i in musics if i.endswith(('.mp3', 'm4a'))]
     ran_music = music_locations[random.randint(0, len(music_locations) - 1)]
     logger.info('Music To Be Played: ' + ran_music)
-    if not play_a_song_via_commandline(ran_music):
-        random_play(musics_location)
+    if mode == 'pygame':
+        if not play_a_song(ran_music):
+            random_play(musics_location, mode=mode)
+        return
+    elif mode == 'commandline':
+        if not play_a_song_via_commandline(ran_music):
+            random_play(musics_location, mode=mode)
 
 
 def play_a_song(music):
@@ -37,8 +51,10 @@ def play_a_song(music):
 
 
 def play_a_song_via_commandline(music):
-    os.system(f'mplayer {music}')
-    return
+    commandline = f'mplayer {music}'
+    logger.info(f'The CommandLine is: {commandline}')
+    os.system(commandline)
+    return True
 
 
 def waitKey():
@@ -50,4 +66,4 @@ def waitKey():
 
 
 if __name__ == '__main__':
-    random_play()
+    random_play(mode='commandline')
