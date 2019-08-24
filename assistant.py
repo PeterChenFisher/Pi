d63f9d05b87e7e27ea55e8c0bd4571604ff5a18a
Peter <peter.chenxiaofeng@gmail.com>
2019-08-24 22:07:37 +0800

    'assistant'

--- assistant.py
+++ assistant.py
@@ -1,5 +1,12 @@
 from Tools import music_play
 from events import schedules
+import sys
 
-# music_play.random_play(musics_location='musics', mode='commandline')
-schedules.weather_reporter()
+if __name__ == '__main__':
+    arg = sys.argv[1]
+    print(arg)
+    print(type(arg))
+    if arg == 'player':
+        music_play.random_play(musics_location='musics', mode='commandline', times=50)
+    elif arg == 'weather':
+        schedules.weather_reporter()
