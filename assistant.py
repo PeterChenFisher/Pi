89832dc8d70a6052f32f17c28dd5b9cc177dd05c
Peter <peter.chenxiaofeng@gmail.com>
2019-08-24 23:00:27 +0800

    'update'

--- assistant.py
+++ assistant.py
@@ -4,7 +4,7 @@ import sys
 
 
 def test():
-    # music_play.reform_music_file_names(musics_location='musics')
+    music_play.reform_music_file_names(musics_location='musics')
     music_play.random_play(musics_location='musics')
     return
 
