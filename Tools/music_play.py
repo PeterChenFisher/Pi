f2b84ee5e93c2e9b51612842c9f0d7a37cb9fc29
Peter <peter.chenxiaofeng@gmail.com>
2019-09-26 21:18:48 +0800

    log

--- Tools/music_play.py
+++ Tools/music_play.py
@@ -30,7 +30,9 @@ def random_play(musics_location=None, mode='commandline', times=1):
     music_locations = [os.path.join(musics_location, i) for i in musics if i.endswith(('.mp3', 'm4a'))]
     music_chains = read_song_list_via_linear_chain(os.path.join(musics_location, 'musics.txt'))
     music_locations.extend(music_chains)
-    logger.info('Musics:%s' % str(music_locations))
+    logger.info('All Musics : %s')
+    for music_location in music_locations:
+        logger.info(str(music_location))
 
     if mode == 'pygame':
         player = play_a_song_via_pygame
