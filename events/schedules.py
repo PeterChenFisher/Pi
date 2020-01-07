79e1cc7959a7dbe71e3b5552dee49cee8ec750a7
Peter <peter.chenxiaofeng@gmail.com>
2020-01-07 21:27:41 +0800

    stop-time-reporting

--- events/schedules.py
+++ events/schedules.py
@@ -14,10 +14,10 @@ def add_block_schedule_jobs(BlockScheduler):
 def add_back_schedule_jobs(BackScheduler):
     BackScheduler.add_job(func=DDingWarn.request_ding, args=([config.heart_beat_text2],), trigger='cron',
                           max_instances=10, month='*', day='*', hour='23', minute='00')
-    BackScheduler.add_job(func=heart_beats.TimeReporting, trigger='cron', max_instances=10, month='*', day='*',
-                          hour='*', minute='00')
-    BackScheduler.add_job(func=heart_beats.TimeReporting, trigger='cron', max_instances=10, month='*', day='*',
-                          hour='*', minute='30')
+    # BackScheduler.add_job(func=heart_beats.TimeReporting, trigger='cron', max_instances=10, month='*', day='*',
+    #                       hour='*', minute='00')
+    # BackScheduler.add_job(func=heart_beats.TimeReporting, trigger='cron', max_instances=10, month='*', day='*',
+    #                       hour='*', minute='30')
     # BackScheduler.add_job(func=DDingWarn.request_ding, args=([config.heart_beat_text2],), trigger='cron',
     #                       max_instances=10,
     #                       month='*', day_of_week='sat,sun', hour='8', minute='49')
