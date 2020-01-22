20f9b7dd56441e7e7c132b49c8e1ab008d0a4831
Peter <peter.chenxiaofeng@gmail.com>
2020-01-22 08:24:01 +0800

    moveSpider-deleteMusic-SimplifyCode-addSocketWait

--- tools/socket_wait.py
+++ tools/socket_wait.py
@@ -6,18 +6,24 @@ import time
 HOST = '127.0.0.1'  # 标准的回环地址 (localhost)
 PORT = 65432  # 监听的端口 (非系统级的端口: 大于 1023)
 Signal = False
-
+Message = ''
+# TODO 设置接收到的信息为 b'message',接收到即检查该信号在信号仓库里是否有存在,若无，返回错误，
+#  若有，将signal设置为True，并将变量Message更改；
+#  signal信号检测函数检测到True，查询Message，执行相关函数。
 logger = log.logger
 
 
-def show_signal():
-    global Signal
+def wait_signal_and_run(func):
+    global signal
+
+    logger.info('正在判斷信號是否為True。。。')
     while True:
         time.sleep(1)
-        if Signal is True:
-            logger.info(f'Signal is: {str(Signal)}')
-            time.sleep(1)
-            Signal = False
+        if signal is True:
+            logger.info(f'Signal is: {signal}')
+            func()
+            time.sleep(5)
+            signal = False
 
 
 def socket_wait():
@@ -39,6 +45,6 @@ def socket_wait():
 
 
 if __name__ == '__main__':
-    show_sig_th = threading.Thread(target=show_signal)
+    show_sig_th = threading.Thread(target=wait_signal_and_run)
     show_sig_th.start()
     socket_wait()
