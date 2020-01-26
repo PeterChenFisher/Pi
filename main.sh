13f1bd53940c702724835af796dde009f89e8816
Peter <peter.chenxiaofeng@gmail.com>
2020-01-26 19:20:31 +0800

    update

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # set up for pi running
-sleep 5
+sleep 10
 cd /home/pi/Projects/Pi/
+git pull gitee master >> ./excluded/gitpull-results.txt
 source /home/pi/Projects/Pi/venv/bin/activate
 python3 /home/pi/Projects/Pi/main.py &
