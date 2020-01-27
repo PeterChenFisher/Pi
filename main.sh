0a6e6d962c60d1c9161500c6954e460b9bdde42e
Peter <peter.chenxiaofeng@gmail.com>
2020-01-27 15:32:48 +0800

    bash-delete-git

--- main.sh
+++ main.sh
@@ -2,6 +2,6 @@
 # set up for pi running
 sleep 10
 cd /home/pi/Projects/Pi/
-git pull gitee master >> ./excluded/gitpull-results.txt
+# sudo git pull gitee master >> ./excluded/gitpull-results.txt
 # source /home/pi/Projects/Pi/venv/bin/activate
 python3 /home/pi/Projects/Pi/main.py &
