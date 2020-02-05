d9e1488c4f6047406b782e9aa8ea08b93d5f7612
Peter <peter.chenxiaofeng@gmail.com>
2020-02-05 16:53:38 +0800

    message

---
+++ ProjAutomation.sh
@@ -0,0 +1,20 @@
+dirname=excluded/git_process
+echo "the dir name is $dirname"
+if [ ! -d $dirname  ];then
+  mkdir $dirname
+else
+  echo dir exist
+fi
+
+date >> excluded/git_process/ProjAuto.log
+echo "Start Project Automation Script" >> excluded/git_process/ProjAuto.log
+git pull gitee master >> excluded/git_process/ProjAuto.log
+PID=$(ps -e|grep python | awk '{printf $1}')
+echo "Main Script PID is: $PID" >> excluded/git_process/ProjAuto.log
+kill -9 ${PID} >> excluded/git_process/ProjAuto.log
+if [ $? -eq 0 ];then
+    echo "kill $PID success" >> excluded/git_process/ProjAuto.log
+else
+    echo "kill $PID fail" >> excluded/git_process/ProjAuto.log
+fi
+bash ./main.sh
