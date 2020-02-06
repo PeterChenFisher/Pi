53a226f1ec8c5e6806c39e78ecfba9b7bf10776b
Peter <peter.chenxiaofeng@gmail.com>
2020-02-06 14:31:19 +0800

    message

--- CodeSyncGitoee.ps1
+++ CodeSyncGitoee.ps1
@@ -1,3 +1,4 @@
 git pull gitee master >> .\excluded\git_process\CodeSync.log
 git commit -am message >> .\excluded\git_process\CodeSync.log
 git push gitee master >> .\excluded\git_process\CodeSync.log
+git push
