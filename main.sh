c87199495686d5bbf93c46bfd5e587aa83e53943
Peter <peter.chenxiaofeng@gmail.com>
2020-04-02 22:17:46 +0800

    .sh-delete-light.py

--- main.sh
+++ main.sh
@@ -7,5 +7,3 @@ echo '开始执行main.py 脚本'
 python3 /home/pi/Projects/Pi/main.py &
 echo '开始执行 树莓派风扇控制 脚本'
 python3 /home/pi/Projects/Pi/events/FansCTR.py &
-echo '开始执行 呼吸灯 脚本'
-python3 /home/pi/Projects/Pi/events/LightBreath.py &
