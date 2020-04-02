98b3ff29ac31fe7e5a754fac5135985aa2b1f0ab
Peter <peter.chenxiaofeng@gmail.com>
2020-04-02 23:24:49 +0800

    debug

--- events/LightBreath.py
+++ events/LightBreath.py
@@ -1,18 +1,19 @@
-import RPi.GPIO as GPIO  # 导入Rpi.GPIO库函数命名为GPIO
-
+from config import *
 import time  # 导入计时time函数
 
-GPIO.setmode(GPIO.BOARD)  # 将GPIO编程方式设置为BOARD模式
-GPIO.setup(11, GPIO.OUT)  # 设置物理引脚11负责输出电压
+if os_platform == 'linux' or os_platform == 'Linux':
+    import RPi.GPIO as GPIO  # 导入Rpi.GPIO库函数命名为GPIO
+
+    GPIO.setmode(GPIO.BOARD)  # 将GPIO编程方式设置为BOARD模式
+    GPIO.setup(11, GPIO.OUT)  # 设置物理引脚11负责输出电压
 
 
-def LightBreath(light_code):
-    while True:
-        GPIO.output(light_code, GPIO.HIGH)
-        time.sleep(1)
-        GPIO.output(light_code, GPIO.LOW)
-        time.sleep(1)
-
+    def LightBreath(light_code):
+        while True:
+            time.sleep(1)
+            GPIO.output(light_code, GPIO.HIGH)
+            time.sleep(1)
+            GPIO.output(light_code, GPIO.LOW)
 
 if __name__ == '__main__':
     LightBreath(11)
