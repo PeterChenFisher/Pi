452d2dc9eceb680246b72874337494e21472be9d
Peter <peter.chenxiaofeng@gmail.com>
2020-05-02 09:46:54 +0800

    delete-music-file

--- events/schedules.py
+++ events/schedules.py
@@ -41,12 +41,9 @@ def add_block_schedule_jobs(BlockScheduler):
 
 
 def add_back_schedule_jobs(BackScheduler):
-    # 每晚十一点报晚安
-    # BackScheduler.add_job(func=DDingWarn.request_ding, args=([heart_beat_text2],), trigger='cron',
-    #                       max_instances=10, month='*', day='*', hour='23', minute='00')
     # 整点提醒功能（每到整点播放纯音乐）
-    # BackScheduler.add_job(func=music_play.random_play, args=(None, 'commandline', 1, pure_music), trigger='cron',
-    #                       max_instances=10, month='*', day='*', hour='*', minute='00')
+    BackScheduler.add_job(func=music_play.random_play, args=(None, 'commandline', 1, pure_music), trigger='cron',
+                          max_instances=10, month='*', day='*', hour='*', minute='00')
     # 每4天重置一次播放器中的音乐列表读取信号reload_sig，使播放器每4天重新获取一次音乐列表
     BackScheduler.add_job(func=music_play.reload_sig_state_switch, trigger='interval', days=4)
     return
