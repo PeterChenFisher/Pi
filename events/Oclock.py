41ea1ca5568a01b9260cb43c125535fe45101078
Peter <peter.chenxiaofeng@gmail.com>
2020-05-08 13:21:07 +0800

    oclock-time-report

--- events/Oclock.py
+++ events/Oclock.py
@@ -8,8 +8,7 @@ def weekday_morning_oclock():
     for i in range(0, music_time):
         music_play.random_play()
         now = time.localtime()
-        hour, minute = now[3], now[4]
-        if hour == 7:
-            heart_beats.time_reporting()
+        # hour, minute = now[3], now[4]
+        heart_beats.time_reporting()
 
     return
