edcebcb27b9eee83de86ff957782d248c19dbf46
Peter <peter.chenxiaofeng@gmail.com>
2020-08-03 21:43:39 +0800

    auto-project-script

--- ProjAutomation.sh
+++ ProjAutomation.sh
@@ -13,8 +13,8 @@ PID=$(ps -e|grep python | awk '{printf $1}')
 echo "Main Script PID is: $PID" >> excluded/git_process/ProjAuto.log
 sudo kill -9 ${PID} >> excluded/git_process/ProjAuto.log
 if [ $? -eq 0 ];then
-    echo "kill $PID success" >> excluded/git_process/ProjAuto.log
+echo "kill $PID success" >> excluded/git_process/ProjAuto.log
+    sudo bash ./main.sh
 else
     echo "kill $PID fail" >> excluded/git_process/ProjAuto.log
 fi
-sudo bash ./main.sh
