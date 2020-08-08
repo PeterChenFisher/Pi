fdc2890b3890cb50a27b66ac1d3b8423666b07f9
Peter <peter.chenxiaofeng@gmail.com>
2020-08-08 18:52:25 +0800

    'logger-reform'

--- tools/log.py
+++ tools/log.py
@@ -12,6 +12,8 @@ third_logger = None
 def logger_generator(logger_name, when='midnight', log_path=None, logger=None):
     # init
     base_logger_path = log_path if log_path else os.path.join(excluded_file, 'log', logger_name)
+    if not os.path.exists(os.path.join(excluded_file, 'log')):
+        os.mkdir(os.path.join(excluded_file, 'log'))
     if not os.path.exists(base_logger_path):
         os.mkdir(base_logger_path)
     # create logger
@@ -20,8 +22,8 @@ def logger_generator(logger_name, when='midnight', log_path=None, logger=None):
     formatter = logging.Formatter(
         '[%(asctime)s] - [ %(name)s ] - %(levelname)s - %(message)s')
     if not logger.handlers:
-        timedRotatingFileHandler = TimedRotatingFileHandler(filename=base_logger_path, when=when,
-                                                            interval=1, backupCount=60, encoding='utf-8')
+        timedRotatingFileHandler = TimedRotatingFileHandler(filename=os.path.join(base_logger_path, logger_name),
+                                                            when=when, interval=1, backupCount=60, encoding='utf-8')
         timedRotatingFileHandler.suffix = "%Y-%m-%d.log"
         timedRotatingFileHandler.setLevel(logging.INFO)
         timedRotatingFileHandler.setFormatter(formatter)
