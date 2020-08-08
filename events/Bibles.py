587fa090793e076d8f84854fe87151d144a57a0f
Peter <peter.chenxiaofeng@gmail.com>
2020-08-08 16:58:35 +0800

    update-split-IPMonitor-DailyScript

--- events/Bibles.py
+++ events/Bibles.py
@@ -2,8 +2,6 @@ from tools import log
 from tools.reply_template import *
 from Spiders.jdjzww_daily import *
 from tools import ServerChanWarn, DDingWarn
-from sched import scheduler
-import time
 
 logger = log.logger
 
@@ -13,20 +11,11 @@ def send_today_scripture():
     update_url_list()
     scripture = get_very_day_scripture()
     logger.info('今日经文请求钉钉！')
-    # result1 = ServerChanWarn.server_chan_post(server_chan_url=Dingding.server_chan_url_wn, title='今日经文',
-    #                                           content=str(scripture))
-    # result2 = ServerChanWarn.server_chan_post(server_chan_url=Dingding.server_chan_url_vinky, title='今日经文',
-    #                                           content=str(scripture))
-    # DDingWarn.request_ding(result=[str(scripture), f'Vinky:{result1[key_message]} \nWN:{result2[key_message]}'])
+    result1 = ServerChanWarn.server_chan_post(server_chan_url=Dingding.server_chan_url_wn, title='今日经文',
+                                              content=str(scripture))
+    result2 = ServerChanWarn.server_chan_post(server_chan_url=Dingding.server_chan_url_vinky, title='今日经文',
+                                              content=str(scripture))
+    DDingWarn.request_ding(result=[str(scripture), f'Vinky:{result1[key_message]} \nWN:{result2[key_message]}'])
     DDingWarn.request_ding(result=[str(scripture)])
     logger.info(f'爬取每日经文结束。')
     return
-
-
-def daily_scripture():
-
-    return
-
-
-if __name__ == '__main__':
-    daily_scripture()
