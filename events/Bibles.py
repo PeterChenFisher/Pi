065d0a3bce015da29081df86a4eda7ccdc64d13e
Peter <peter.chenxiaofeng@gmail.com>
2020-08-22 14:43:41 +0800

    'DS'

--- events/Bibles.py
+++ events/Bibles.py
@@ -1,4 +1,3 @@
-from tools import log
 from tools.reply_template import *
 from Spiders.jdjzww_daily import *
 from tools import ServerChanWarn, DDingWarn
@@ -11,8 +10,10 @@ def send_today_scripture():
     update_url_list()
     scripture = get_very_day_scripture()
     logger.info('今日经文请求钉钉！')
+    logger.info('发送给卫娜')
     result1 = ServerChanWarn.server_chan_post(server_chan_url=Dingding.server_chan_url_wn, title='今日经文',
                                               content=str(scripture))
+    logger.info('发送给温琦')
     result2 = ServerChanWarn.server_chan_post(server_chan_url=Dingding.server_chan_url_vinky, title='今日经文',
                                               content=str(scripture))
     DDingWarn.request_ding(result=[str(scripture), f'Vinky:{result1[key_message]} \nWN:{result2[key_message]}'])
