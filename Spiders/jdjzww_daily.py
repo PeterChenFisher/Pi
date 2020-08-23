5e98827d1b8b27d81aae3f977961f66ad40ea363
Peter <peter.chenxiaofeng@gmail.com>
2020-08-23 09:22:32 +0800

    'ds-bug'

--- Spiders/jdjzww_daily.py
+++ Spiders/jdjzww_daily.py
@@ -14,6 +14,7 @@ def get_scripture(url):
     # soup = BeautifulSoup(resp.text, 'html5lib')
     soup = BeautifulSoup(resp.text)
     scripture = soup.find('div', class_='texts').p.next
+    scripture = scripture.text
     return scripture
 
 
